e43de25f29791c46790fa04cbf9adbaf14e6f54b
Mauli <aishwaryacmadrewar@gmail.com>
2024-02-15 14:51:41 +0530

    first commit

---
+++ src/components/ProjectsSection.js
@@ -0,0 +1,63 @@
+import React from "react";
+import FullScreenSection from "./FullScreenSection";
+import { Box, Heading } from "@chakra-ui/react";
+import Card from "./Card";
+
+const projects = [
+  {
+    title: "React Calculator Web APPlication",
+    description:
+      "React Calculator application that allows users to perform basic mathematical operations. The project is designed to be a simple introduction to React and its components. The project is made up of two main components: the Calculator component and the Button component. The Calculator component is responsible for displaying the calculator interface and handling user input.",
+    getImageSrc: () => require("../images/react calculator project.png"),
+  },
+  {
+    title: "Little-Lemon Web Application",
+    description:
+      "A table-booking system using React. It's a fully responsive web app that allows users to reserve a table for the Little Lemon restaurant.🔥️",
+    getImageSrc: () => require("../images/photo2.jpg"),
+  },
+  {
+    title: "Photo Gallery",
+    description:
+      "A One-stop shop for photographers to share and monetize their photos, allowing them to have a second source of income",
+    getImageSrc: () => require("../images/photo3.jpg"),
+  },
+  {
+    title: "Course Learning Platform",
+    description: "",
+      
+    getImageSrc: () => require("../images/photo4.jpg"),
+  },
+];
+
+const ProjectsSection = () => {
+  return (
+    <FullScreenSection
+      backgroundColor="#14532d"
+      isDarkBackground
+      p={8}
+      alignItems="flex-start"
+      spacing={8}
+    >
+      <Heading as="h1" id="projects-section">
+        Featured Projects
+      </Heading>
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(2,minmax(0,1fr))"
+        gridGap={8}
+      >
+        {projects.map((project) => (
+          <Card
+            key={project.title}
+            title={project.title}
+            description={project.description}
+            imageSrc={project.getImageSrc()}
+          />
+        ))}
+      </Box>
+    </FullScreenSection>
+  );
+};
+
+export default ProjectsSection;
